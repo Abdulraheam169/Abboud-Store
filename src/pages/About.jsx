987dc731f623e2a977bce2f72649452fceda2dfb
Abdulraheam169 <abdulraheam.169@gmail.com>
2026-05-22 07:18:51 +0300

    Final path fix :)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-
 export default function About() {
   return (
     <div className="about-me">
-      <img
-        className="a-img"
-        src="../images/personal.png"
-        alt=""
-        width={"100px"}
-      />
+      <img className="a-img" src="public/personal.png" alt="" width={"100px"} />
       <h1 className="a-name">Abd Al-Raheam Al-Ruqaya</h1>
 
       <ul className="a-personal">
-        <h3>personal</h3>
+        <h3>Personal Info</h3>
         <li>age: 22</li>
         <li>address: Syria - Damascus</li>
         <li>
@@ -23,13 +17,12 @@ export default function About() {
         </li>
       </ul>
       <ul className="a-info">
-        <h3>info </h3>
+        <h3>Academic Info </h3>
         <li>ITE Student at SVU</li>
-        <li> Fresh Front-End Developer</li>
       </ul>
 
       <ul className="a-skills">
-        <h3>Skilles</h3>
+        <h3>Skills</h3>
         <li>HTML</li>
         <li>Java Script</li>
         <li>CSS</li>
@@ -37,10 +30,14 @@ export default function About() {
         <li>git - github</li>
       </ul>
       <ul className="a-projects">
-        <h3>My Projects</h3>
+        <h3>Some Projects</h3>
         <li>
-          <a target="_blank" href="https://abdulraheam169.github.io/Hangman-/">
-            <img src="../images/hangman.webp" alt="" />
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://abdulraheam169.github.io/Hangman-/"
+          >
+            <img src="public/hangman.webp" alt="" />
             <span>Hangman Game</span>
           </a>
         </li>
@@ -48,31 +45,41 @@ export default function About() {
         <li>
           <a
             target="_blank"
+            rel="noopener noreferrer"
             href="https://abdulraheam169.github.io/html-css-template-1/"
           >
-            <img src="../images/template.webp" alt="" />
+            <img src="public/template.webp" alt="" />
             <span>HTML/CSS Template 1</span>
           </a>
         </li>
         <li>
           <a
             target="_blank"
+            rel="noopener noreferrer"
             href="https://abdulraheam169.github.io/html-css-Template-3/"
           >
-            <img src="../images/template-2.webp" alt="" />
+            <img src="public/template-2.webp" alt="" />
             <span>HTML/CSS Template 2</span>
           </a>
         </li>
 
         <li>
-          <a target="_blank" href="">
-            <img src="../images/dice.jpg" alt="" />
+          <a target="_blank" rel="noopener noreferrer" href="">
+            <img src="public/dice.jpg" alt="" />
             <span>Tenzies</span>
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://abdulraheam169.github.io/ToDo-List/">
-            <img src="../images/to-do-list-icon-vector.webp" alt="" />
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://abdulraheam169.github.io/ToDo-List/"
+          >
+            <img
+              src="
+            public/to-do-list-icon-vector.webp"
+              alt=""
+            />
             <span>ToDo List</span>
           </a>
         </li>
