d093b82b93b2dd520d542df1b8e95b99b159fb53
Abdulraheam169 <abdulraheam.169@gmail.com>
2026-05-23 12:08:54 +0300

    fix the delay

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Abboud-Store/",
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: true,
+      },
+    },
+  },
 });
